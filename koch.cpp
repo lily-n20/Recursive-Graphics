413c650f890dc0643a6d8c86c06935e691df7177
ilanna-l <113138033+ilanna-l@users.noreply.github.com>
2023-12-03 23:32:55 -0500

    Add files via upload
    Signed-off-by: ilanna-l <113138033+ilanna-l@users.noreply.github.com>

--- koch.cpp
+++ koch.cpp
@@ -7,26 +7,32 @@
 // + = turn left 60
 // - = turn right 60
 
+//recursive function to draw single koch curve
 std::string koch_curve(int level){
+    //base level of complexity 
     if(level == 1){
         return "F ";
-        
     }
-    //
+    //split line into four parts with middle two lines connecting upwards to create a equilateral triangle with no base
     return koch_curve(level - 1) + "+ " + koch_curve(level - 1) + "- - " + koch_curve(level - 1) + "+ " + koch_curve(level - 1);
 }
 
 
 int main(int argc, char* argv[]) {
-
-    //take in evel (level of the drawing) and outfile string name
+    //take in level of complexity
     int depth = std::stof(argv[1]);
+    //create output file
     std::ofstream L_string("l-system.txt");
 
+    //exception handling
+    if(depth < 1){
+        depth = 1;
+    }
 
+    //call curve three times to create snowflake
     for (int i = 0 ; i < 3 ; i++) {
         L_string << koch_curve(depth);
-        L_string << "- - ";
+        L_string << "- - "; //turn right 120 degrees before next line
     }
 
 }
